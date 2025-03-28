67814cd155ef4a491b3bbb4328e665f617db3b31
Kenéz <kenez.prime@gmail.com>
2025-03-28 20:21:38 +0100

    Working on bayesian modelling, also decided to only have one "functions" script, which will contain functions that can be sourced later.

--- scripts/functions.R
+++ scripts/functions.R
@@ -1,4 +1,4 @@
-# Function to calculate net benefits
+### Function to calculate net benefits
 calculate_net_benefits <- function(df, thresholds = seq(0, 1, by = 0.01), probabilities_column, outcomes_column) {
   net_benefits <- numeric(length(thresholds))
   
@@ -27,3 +27,14 @@ calculate_net_benefits <- function(df, thresholds = seq(0, 1, by = 0.01), probab
   data.frame(Threshold = thresholds, NetBenefit = net_benefits)
 }
 
+### Function to log runtimes 
+log_runtime <- function(start_time, end_time, model_name, log_file_with_path) {
+  # Calculate the time difference
+  runtime <- end_time - start_time
+  
+  # Format the log message
+  log_message <- paste(Sys.time(), "| Model:", model_name, "| Runtime:", runtime, "seconds")
+  
+  # Append the log message to the text file
+  write(log_message, file = log_file_with_path, append = TRUE)
+}
